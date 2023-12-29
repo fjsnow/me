4fb55c720abe30aee8b26a6452ce49da11fcdd0c
Freddy Snow <fjsn@protonmail.com>
2023-12-29 08:21:34 +0000

    Accidently broke everything lol

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <html lang="en" className={dark ? "dark" : ""}>
             <body
                 className={twMerge(
-                    "screenflex justify-center bg-white text-black dark:bg-black dark:text-white",
+                    "screen flex justify-center bg-white text-black dark:bg-black dark:text-white",
                     ibm.className
                 )}
             >
