684e1ebb020e12246266b28bbad1decd39b9928d
Freddy Snow <fjsn@protonmail.com>
2024-01-30 21:48:40 +0000

    Fix birthdate again. Cause was diff in format between US/The rest of the world :D

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,7 +74,7 @@ const ShortTechnology = ({
 };
 
 const Index = () => {
-    const birth = "2004-12-03";
+    const birth = "2004-12-03T00:00:00.000Z";
     const age = Math.floor(
         (new Date().getTime() - new Date(birth).getTime()) /
             (1000 * 60 * 60 * 24 * 365)
