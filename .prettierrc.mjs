679c5fa568d4844c9fc8f6de03a31abff4648eb9
Freddy Snow <fjsn@protonmail.com>
2024-12-21 18:52:27 +0000

    Prettier

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,13 +1,14 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
-  ],
+    tabWidth: 4,
+    plugins: ["prettier-plugin-astro"],
+    overrides: [
+        {
+            files: "*.astro",
+            options: {
+                parser: "astro",
+            },
+        },
+    ],
 };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,53 +1,53 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      typography: {
-        DEFAULT: {
-          css: {
-            color: '#000000',
-            h1: {
-              marginTop: 0,
-              marginBottom: 0,
-              fontWeight: 500,
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            typography: {
+                DEFAULT: {
+                    css: {
+                        color: "#000000",
+                        h1: {
+                            marginTop: 0,
+                            marginBottom: 0,
+                            fontWeight: 500,
+                        },
+                        h2: {
+                            marginTop: 0,
+                            marginBottom: 0,
+                            fontWeight: 500,
+                        },
+                        h3: {
+                            marginTop: 0,
+                            marginBottom: 0,
+                            fontWeight: 500,
+                        },
+                        h4: {
+                            marginTop: 0,
+                            marginBottom: 0,
+                            fontWeight: 500,
+                        },
+                        h5: {
+                            marginTop: 0,
+                            marginBottom: 0,
+                            fontWeight: 500,
+                        },
+                        h6: {
+                            marginTop: 0,
+                            marginBottom: 0,
+                            fontWeight: 500,
+                        },
+                        p: {
+                            marginTop: 0,
+                            marginBottom: "1rem",
+                        },
+                        b: {
+                            fontWeight: 700,
+                        },
+                    },
+                },
             },
-            h2: {
-              marginTop: 0,
-              marginBottom: 0,
-              fontWeight: 500,
-            },
-            h3: {
-              marginTop: 0,
-              marginBottom: 0,
-              fontWeight: 500,
-            },
-            h4: {
-              marginTop: 0,
-              marginBottom: 0,
-              fontWeight: 500,
-            },
-            h5: {
-              marginTop: 0,
-              marginBottom: 0,
-              fontWeight: 500,
-            },
-            h6: {
-              marginTop: 0,
-              marginBottom: 0,
-              fontWeight: 500,
-            },
-            p: {
-              marginTop: 0,
-              marginBottom: '1rem',
-            },
-            b: {
-              fontWeight: 700,
-            },
-          },
         },
-      },
     },
-  },
-  plugins: [require('@tailwindcss/typography')],
+    plugins: [require("@tailwindcss/typography")],
 };
